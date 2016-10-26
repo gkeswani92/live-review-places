9b28761207a4fc5a2688e2a035ac5ce6cc467789
gkeswani92 <gauravkeswani92@gmail.com>
2016-10-26 00:38:21 -0700

    Bug fix to add GET and POST request functionality to sign up endpoint

--- api_cmds/routes.py
+++ api_cmds/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from flask import Blueprint
+from flask import request
 from forms. sign_up import SignUpForm
 
 
@@ -18,7 +19,13 @@ def about():
     return render_template('about.html')
 
 
-@sign_up_page.route('/signup')
+@sign_up_page.route('/signup', methods=['GET', 'POST'])
 def sign_up():
+
+    # If the request method is POST, it means that the request has come from the clicking of the submit button
+    if request.method == 'POST':
+        return "Successfully submitted form"
+
+    # Get request means this is a fresh request and is not coming from clicking the submit button
     form = SignUpForm()
     return render_template('sign_up.html', form=form)
