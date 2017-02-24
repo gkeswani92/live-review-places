6b35cdecc9b6fb006a6f23e76d402702997c3400
gkeswani92 <gauravkeswani92@gmail.com>
2017-02-24 13:56:47 -0500

    #4 Added a testrunner file that runs the entire test suite and used that in .travis.yml

---
+++ testrunner.py
@@ -0,0 +1,29 @@
+import optparse
+import sys
+import unittest
+
+
+USAGE = """%prog TEST_PATH
+Run unit tests for Map Nearby Places App.
+TEST_PATH   Path to package containing test modules"""
+
+
+def run_test_suite(test_path):
+    # Discover and run tests.
+    suite = unittest.loader.TestLoader().discover(test_path)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+
+    # Fail the build if the suite was not successful!
+    if not result.wasSuccessful():
+        sys.exit(result)
+
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser(USAGE)
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        print 'Error: Exactly 2 arguments required.'
+        parser.print_help()
+        sys.exit(1)
+    TEST_PATH = args[0]
+    run_test_suite(TEST_PATH)
