b5f6cd9a076e17fca12fa8f01babee22e309f70a
gkeswani92 <gauravkeswani92@gmail.com>
2017-02-24 01:20:25 -0500

    Moved default nearby to wikipedia nearby class in preparation of google places api calls

--- api_cmds/routes.py
+++ api_cmds/routes.py
@@ -5,7 +5,7 @@ from forms.login import LoginForm
 from forms.address import AddressForm
 from logic.sign_up import register_user
 from logic.sign_in import is_valid_user
-from logic.nearby import find_nearby_places, address_to_latlng
+from logic.nearby import NearbyWikipedia, address_to_latlng
 
 
 index_page = Blueprint('index', __name__, template_folder='templates')
@@ -69,7 +69,7 @@ def home():
         else:
             address = form.address.data
             my_coordinates = address_to_latlng(address)
-            nearby_places = find_nearby_places(address)
+            nearby_places = NearbyWikipedia.find_nearby_places(address)
             return render_template('home.html', form=form, places=nearby_places, my_coordinates=my_coordinates)
 
     else:
--- logic/nearby.py
+++ logic/nearby.py
@@ -19,13 +19,6 @@ def meters_to_walking_time(meters):
     return -1
 
 
-def wiki_path(place):
-    """
-    Creates the wikipedia link for the place
-    """
-    return urllib2.urlparse.urljoin("http://en.wikipedia.org/wiki/", place.replace(' ', '_'))
-
-
 def address_to_latlng(address):
     """
     Finds the latitude and longitude of the address
@@ -34,44 +27,54 @@ def address_to_latlng(address):
     return g.lat, g.lng
 
 
-def find_nearby_places(address):
-    """
-    Finds near by places for the given address by using wikipedia's api
-    :param address:
-    :return:
-    """
-    latitude, longitude = address_to_latlng(address)
+class NearbyWikipedia(object):
 
-    api_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=20&format=json'
-    query_url = api_url.format(latitude, longitude)
+    @staticmethod
+    def wiki_path(place):
+        """
+        Creates the wikipedia link for the place
+        """
+        return urllib2.urlparse.urljoin("http://en.wikipedia.org/wiki/", place.replace(' ', '_'))
 
-    query = urllib2.urlopen(query_url)
-    results = query.read()
-    query.close()
+    @staticmethod
+    def find_nearby_places(address):
+        """
+        Finds near by places for the given address by using wikipedia's api
+        :param address:
+        :return:
+        """
+        latitude, longitude = address_to_latlng(address)
 
-    data = json.loads(results)
+        api_url = 'https://en.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=20&format=json'
+        query_url = api_url.format(latitude, longitude)
 
-    places = []
-    for place in data['query']['geosearch']:
-        name = place['title']
-        meters = place['dist']
-        latitude = place['lat']
-        longitude = place['lon']
+        query = urllib2.urlopen(query_url)
+        results = query.read()
+        query.close()
 
-        wiki_url = wiki_path(name)
-        walking_time = meters_to_walking_time(meters)
+        data = json.loads(results)
 
-        place_info = {
-            'name': name,
-            'url': wiki_url,
-            'time': walking_time,
-            'latitude': latitude,
-            'longitude': longitude
-        }
+        places = []
+        for place in data['query']['geosearch']:
+            name = place['title']
+            meters = place['dist']
+            latitude = place['lat']
+            longitude = place['lon']
 
-        places.append(place_info)
+            wiki_url = NearbyWikipedia.wiki_path(name)
+            walking_time = meters_to_walking_time(meters)
 
-    return places
+            place_info = {
+                'name': name,
+                'url': wiki_url,
+                'time': walking_time,
+                'latitude': latitude,
+                'longitude': longitude
+            }
+
+            places.append(place_info)
+
+        return places
 
 
 
